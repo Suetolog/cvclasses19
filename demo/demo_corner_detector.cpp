8536a41c3edfcaba739b3cf1981d12049f3736cd
Suetolog <artem.kriushov@mail.ru>
2022-10-29 18:34:09 +0300

    lab4 done
    lab4 done

--- demo/demo_corner_detector.cpp
+++ demo/demo_corner_detector.cpp
@@ -22,19 +22,26 @@ int demo_corner_detector(int argc, char* argv[])
     cv::namedWindow(demo_wnd);
 
     cv::Mat frame;
-    auto detector = cv::GFTTDetector::create(); // \todo use cvlib::corner_detector_fast
+    auto detector = cvlib::corner_detector_fast::create(); // \todo use cvlib::corner_detector_fast
     std::vector<cv::KeyPoint> corners;
 
     utils::fps_counter fps;
     while (cv::waitKey(30) != 27) // ESC
     {
         cap >> frame;
+        cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
         cv::imshow(main_wnd, frame);
+
+        cv::blur(frame, frame, cv::Size(5, 5)); //Дополнительно добавлено гауссово сглаживания для устранения шумов
 
         detector->detect(frame, corners);
         cv::drawKeypoints(frame, corners, frame, cv::Scalar(0, 0, 255));
         utils::put_fps_text(frame, fps);
         // \todo add count of the detected corners at the top left corner of the image. Use green text color.
+        
+        std::stringstream ss;
+        ss << "Corners: " << corners.size();
+        cv::putText(frame, ss.str(), cv::Point(20, 20), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 255, 0));
         cv::imshow(demo_wnd, frame);
     }
 
